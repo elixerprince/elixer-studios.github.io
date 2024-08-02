66f27f7699e56ec8344f83774fe54e9fc2d0235a
Elixer Prince <dleith@stu.themico.edu.jm>
2024-08-02 01:12:55 -0500

    fixed navbar functionality on mobiles

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,15 +1,18 @@
 "use strict";
-const hamburgerIcon = document.querySelector("#hamburg-icon");
-const mobileNavbar = document.querySelector("#mobile-navbar");
-const closeButton = document.querySelector("#close-btn");
-const openMenu = () => {
-    hamburgerIcon.classList.add("hidden");
-    mobileNavbar.classList.remove("hidden");
-    console.log("Hamburger clicked");
-};
-const hideMenu = () => {
-    mobileNavbar.classList.add("hidden");
-    hamburgerIcon.classList.remove("hidden");
-};
-hamburgerIcon.addEventListener("click", openMenu);
-closeButton.addEventListener("click", hideMenu);
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerIcon = document.querySelector("#hamburg-icon");
+    const mobileNavbar = document.querySelector("#mobile-navbar");
+    const closeButton = document.querySelector("#close-btn");
+    alert("Hello World");
+    const openMenu = () => {
+        hamburgerIcon.classList.add("hidden");
+        mobileNavbar.classList.remove("hidden");
+        console.log("Hamburger clicked");
+    };
+    const hideMenu = () => {
+        mobileNavbar.classList.add("hidden");
+        hamburgerIcon.classList.remove("hidden");
+    };
+    hamburgerIcon.addEventListener("click", openMenu);
+    closeButton.addEventListener("click", hideMenu);
+});
