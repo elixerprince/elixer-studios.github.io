328206598a5805c8bdcfca25f13cdea3f1a46fc2
Elixer Prince <dleith@stu.themico.edu.jm>
2024-08-01 23:10:47 -0500

    Added extra files

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-const hamburgerIcon: any = document.querySelector("#hamburg-icon");
-const mobileNavbar: any = document.querySelector("#mobile-navbar");
-const closeButton:any = document.querySelector("#close-btn");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerIcon: any = document.querySelector("#hamburg-icon");
+  const mobileNavbar: any = document.querySelector("#mobile-navbar");
+  const closeButton: any = document.querySelector("#close-btn");
 
-const openMenu = () => {
-  hamburgerIcon.classList.add("hidden");
-  mobileNavbar.classList.remove("hidden");
-  console.log("Hamburger clicked");
-}
+  const openMenu = () => {
+    hamburgerIcon.classList.add("hidden");
+    mobileNavbar.classList.remove("hidden");
+    console.log("Hamburger clicked");
+  };
 
-const hideMenu = () => {
-  mobileNavbar.classList.add("hidden");
-  hamburgerIcon.classList.remove("hidden");
-}
+  const hideMenu = () => {
+    mobileNavbar.classList.add("hidden");
+    hamburgerIcon.classList.remove("hidden");
+  };
 
-hamburgerIcon.addEventListener("click", openMenu);
-closeButton.addEventListener("click", hideMenu)
+  hamburgerIcon.addEventListener("click", openMenu);
+  closeButton.addEventListener("click", hideMenu);
+});
